9a234805910e4488a3f9f7257a0735d7e9a03778
Thomas <t.lu@email.com>
2019-12-04 00:59:59 -0800

    cluster solver update

--- cluster.py
+++ cluster.py
@@ -1,8 +1,9 @@
 
-def cluster(G):
+def cluster(G, vertices):
     """
     Input:
         G: graph of the nodes we want to cluster
+        vertices: cluster the vertices in vertices to two groups
         k: always just do 2??
     Output:
         2 lists for the different clusters
@@ -10,13 +11,13 @@ def cluster(G):
 
     pass
 
-def findMiddle():
+def findMiddle(G, nodes):
     """
     Input:
         G: graph of all the nodes/edges
         nodes: we want to find the node thats most at the middle of this list
     Output:
-        the middlest node index
+        the node in G that is most at the middle of nodes
     """
     # maybe try an O(n^2) thing, find the sum of distances, and pick the node that has the lowest of these sums
     pass
--- cluster_solver.py
+++ cluster_solver.py
@@ -1,3 +1,8 @@
+import random
+import numpy as np
+import networkx as nx
+from cluster import cluster, findMiddle
+
 def solve(list_of_homes, starting_car_location, G, params=[]):
     """
     Write your algorithm here.
@@ -10,7 +15,56 @@ def solve(list_of_homes, starting_car_location, G, params=[]):
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
-    # create graph of only homes
-    # 
+    curr_clusters = [list_of_homes]
+    curr_centers = [findMiddle(G, list_of_homes)]
+
+    new_clusters = []
+    for verts in curr_clusters:
+        new_clusters += cluster(G, verts)
+    new_clusters = [findMiddle(G, clus) for clus in new_clusters]
+    
+    while (len(curr_centers) != len(new_centers)):
+        pass
     
     pass
+
+def travelingSalesman(G, vertices, start):
+    """
+    Approximation algo for tsp modified from 
+    https://ericphanson.com/blog/2016/the-traveling-salesman-and-10-lines-of-python/
+
+    Input:
+        G: The graph on which to tsp
+        vertices: a list of the vertices on which to do tsp. start only appears once
+        start: the vertex on which to start and end the tsp
+    Output:
+        A list of vertices to visit in order of visitation
+    """
+    vertices.remove(start)
+    random.shuffle(vertices)
+    for temperature in np.logspace(0,6,num=100000)[::-1]:
+        [i,j] = sorted(random.sample(range(len(vertices)),2))
+
+        old = getWeight(G, i, i+1, vertices) + getWeight(G, j-1, j, vertices)
+        new = getWeight(G, j, i+1, newTour) + getWeight(G, j-1, i, newTour)
+        
+        if i == 0:
+            old += getWeight(G, start, i, vertices)
+            new += getWeight(G, start, j, newTour)
+        else:
+            old += getWeight(G, i - 1, i, vertices)
+            new += getWeight(G, i - 1, j, newTour)
+
+        if j == len(vertices) - 1:
+            old += getWeight(G, j, start, vertices)
+            new += getWeight(G, i, start, newTour)
+        else:
+            old += getWeight(G, j, j+1, vertices)
+            new += getWeight(G, i, j+1, newTour)
+
+        if math.exp( ( old - new) / temperature) > random.random():
+            vertices = vertices[:i] + vertices[j:j+1] +  vertices[i+1:j] + vertices[i:i+1] + vertices[j+1:]
+    return [start] + vertices + [start]
+
+def getWeight(G, i, j, vertices):
+    return G.get_edge_data(i, j)["weight"]
