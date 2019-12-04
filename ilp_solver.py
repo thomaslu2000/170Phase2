0a575781064e83c96d53d5f69eecf482a1276f9d
Elicia <eliciaye@gmail.com>
2019-12-03 23:25:17 -0800

    ILP

--- ilp_solver.py
+++ ilp_solver.py
@@ -16,12 +16,26 @@ def solve(list_of_homes, starting_car_location, G, params=[]):
     # create graph of only homes
 
     prob = LpProblem("Drive TAs Home", LpMinimize)
+    newG = nx.DiGraph()
+    newG.add_nodes_from(G)
+
     for edge in G.edges():
-        de = G.edge[edge[0]][edge[1]]['weight'] # double check this
+        fro = edge[0]
+        to = edge[1]
+        de = G.edge[fro][to]]['weight']
+
         rleft = pulp.LpVariable('rleft', lowBound=0, upBound = 1, cat='Integer')
+        newG.add_edge(fro, to, rleft)
+
         rright = pulp.LpVariable('rright', lowBound=0, upBound = 1, cat='Integer')
+        newG.add_edge(to, fro, rright)
+
         tleft = pulp.LpVariable('tleft', lowBound=0, cat='Integer')
+        newG.add_edge(fro, to, tleft)
+
         tright = pulp.LpVariable('tright', lowBound=0, cat='Integer')
+        newG.add_edge(to, fro, tright)
+
         prob += de * ((rleft + rright) * 2 / 3 + tleft + tright), "Total Cost of Path"
         # G.edge[edge[0]][edge[1]]['weight']
     prob.solve()
